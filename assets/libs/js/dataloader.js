9d79c861f6cf2a83c384ff127a8992074e0326f9
Jeshreen <jeshreen.balraj@itelasoft.com>
2022-01-04 07:09:06 +0530

    q1 completion

--- assets/libs/js/dataloader.js
+++ assets/libs/js/dataloader.js
@@ -1,12 +1,23 @@
 $(function() {
-    $("#delayed-flights").html("500000");
-    debugger;
+   
 
     //question 1
-    jQuery.get('data/q1-hadoop.txt', function(data) {
-        alert(data);
-        //process text file line by line
-        // $('#div').html(data.replace('n',''));
+    jQuery.get('https://raw.githubusercontent.com/Jeshreen/concept/master/assets/data/q1-hadoop.txt', function(data) {
+        console.log(data);
+        var lines = data.split('\n');
+        console.log("lines",lines);
+       
+        //Delayed flights
+        var delay = lines[1].split('\t');
+        $("#delayed-flights").html(delay[1]);
+       
+        //Cancelled flights
+        var cancelled = lines[0].split('\t');
+        $("#cancelled-flights").html(cancelled[1]);
+        
+        //ontime flights
+        var ontime = lines[2].split('\t');
+        $("#ontime-flights").html(ontime[1]);
     });
 });
 
